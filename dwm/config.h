c1dfd1ffd4d555ccd5abcb32233703da35fa465d
EfeTheCaptain <m.tahaefe@hotmail.com>
2025-03-18 11:15:02 +0300

    changed the color scheme

--- dwm/config.h
+++ dwm/config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "/home/efe/suckless/dwm/themes/ground-theme.h"
+
 /* Helper macros for spawning commands */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 #define CMD(...)   { .v = (const char*[]){ __VA_ARGS__, NULL } }
@@ -31,48 +33,6 @@ static const int ulineall = 0;                  /* 1 to show underline on all ta
 /* Indicators: see patch/bar_indicators.h for options */
 static const char *fonts[]               = { "DroidSansMNerdFont:size=9" };
 static const char dmenufont[]            = "DroidSansMNerdFont:size=9";
-
-static char c000000[]                    = "#000000"; // placeholder value
-
-static char normfgcolor[]                = "#bbbbbb";
-static char normbgcolor[]                = "#222222";
-static char normbordercolor[]            = "#444444";
-static char normfloatcolor[]             = "#db8fd9";
-
-static char selfgcolor[]                 = "#eeeeee";
-static char selbgcolor[]                 = "#005577";
-static char selbordercolor[]             = "#005577";
-static char selfloatcolor[]              = "#005577";
-
-static char titlenormfgcolor[]           = "#bbbbbb";
-static char titlenormbgcolor[]           = "#222222";
-static char titlenormbordercolor[]       = "#444444";
-static char titlenormfloatcolor[]        = "#db8fd9";
-
-static char titleselfgcolor[]            = "#eeeeee";
-static char titleselbgcolor[]            = "#005577";
-static char titleselbordercolor[]        = "#005577";
-static char titleselfloatcolor[]         = "#005577";
-
-static char tagsnormfgcolor[]            = "#bbbbbb";
-static char tagsnormbgcolor[]            = "#222222";
-static char tagsnormbordercolor[]        = "#444444";
-static char tagsnormfloatcolor[]         = "#db8fd9";
-
-static char tagsselfgcolor[]             = "#eeeeee";
-static char tagsselbgcolor[]             = "#005577";
-static char tagsselbordercolor[]         = "#005577";
-static char tagsselfloatcolor[]          = "#005577";
-
-static char hidnormfgcolor[]             = "#005577";
-static char hidselfgcolor[]              = "#227799";
-static char hidnormbgcolor[]             = "#222222";
-static char hidselbgcolor[]              = "#222222";
-
-static char urgfgcolor[]                 = "#bbbbbb";
-static char urgbgcolor[]                 = "#222222";
-static char urgbordercolor[]             = "#ff0000";
-static char urgfloatcolor[]              = "#db8fd9";
 
 static char *colors[][ColCount] = {
 	/*                       fg                bg                border                float */
---
+++ dwm/themes/ground-theme.h
@@ -0,0 +1,49 @@
+// Updated color definitions using a dark green, brown, and yellow color scheme
+
+static char c000000[]                    = "#000000"; // placeholder value
+
+// Normal colors
+static char normfgcolor[]                = "#d5c39a"; // Light brown/yellow for normal text
+static char normbgcolor[]                = "#2b2b2b"; // Dark brown background
+static char normbordercolor[]            = "#3e3e3e"; // Slightly lighter brown for borders
+static char normfloatcolor[]             = "#b69b6a"; // Light tan/brown
+
+// Selected colors
+static char selfgcolor[]                 = "#fbe3bb"; // Light yellow for selected text
+static char selbgcolor[]                 = "#005f00"; // Dark green background for selection
+static char selbordercolor[]             = "#005f00"; // Dark green border for selection
+static char selfloatcolor[]              = "#005f00"; // Same dark green for float windows
+
+// Title colors
+static char titlenormfgcolor[]           = "#d5c39a"; // Light brown/yellow for normal title text
+static char titlenormbgcolor[]           = "#2b2b2b"; // Dark brown background for title
+static char titlenormbordercolor[]       = "#3e3e3e"; // Slightly lighter brown for title borders
+static char titlenormfloatcolor[]        = "#b69b6a"; // Light tan/brown for title float
+
+static char titleselfgcolor[]            = "#fbe3bb"; // Light yellow for selected title text
+static char titleselbgcolor[]            = "#005f00"; // Dark green background for selected title
+static char titleselbordercolor[]        = "#005f00"; // Dark green border for selected title
+static char titleselfloatcolor[]         = "#005f00"; // Same dark green for selected title float
+
+// Tag colors
+static char tagsnormfgcolor[]            = "#d5c39a"; // Light brown/yellow for normal tags
+static char tagsnormbgcolor[]            = "#2b2b2b"; // Dark brown background for tags
+static char tagsnormbordercolor[]        = "#3e3e3e"; // Slightly lighter brown for tag borders
+static char tagsnormfloatcolor[]         = "#b69b6a"; // Light tan/brown for tag float
+
+static char tagsselfgcolor[]             = "#fbe3bb"; // Light yellow for selected tag text
+static char tagsselbgcolor[]             = "#005f00"; // Dark green for selected tag background
+static char tagsselbordercolor[]         = "#005f00"; // Dark green border for selected tags
+static char tagsselfloatcolor[]          = "#005f00"; // Dark green for selected tag float
+
+// Hidden colors
+static char hidnormfgcolor[]             = "#d5c39a"; // Light brown/yellow for normal hidden text
+static char hidselfgcolor[]              = "#fbe3bb"; // Light yellow for selected hidden text
+static char hidnormbgcolor[]             = "#2b2b2b"; // Dark brown background for hidden
+static char hidselbgcolor[]              = "#2b2b2b"; // Dark brown for selected hidden
+
+// Urgent colors
+static char urgfgcolor[]                 = "#d5c39a"; // Light brown/yellow for normal urgent text
+static char urgbgcolor[]                 = "#2b2b2b"; // Dark brown background for urgent
+static char urgbordercolor[]             = "#ffcc00"; // Bright yellow for urgent borders
+static char urgfloatcolor[]              = "#b69b6a"; // Light tan/brown for urgent float
