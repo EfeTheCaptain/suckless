9c0f637c902b197c004877278f099925a7226855
EfeTheCaptain <m.tahaefe@hotmail.com>
2025-03-18 09:52:54 +0300

    theme

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -24,8 +24,8 @@
 	X("", "/home/efe/.local/bin/cpu-usage.sh", 2, 16)\
 	X("", "/home/efe/.local/bin/volume.sh", 0, 15)\
 	X("", "/home/efe/.local/bin/battery.sh", 10, 14)\
-	X("", "/home/efe/.local/bin/date.sh", 3600, 13)\
 	X("", "/home/efe/.local/bin/weather.sh", 3600, 12)\
+	X("", "/home/efe/.local/bin/date.sh", 3600, 13)\
 	X("", "/home/efe/.local/bin/time.sh", 60, 11)\
 	X("", "/home/efe/.local/bin/wifi.sh", 30, 10)	 
 #endif  // CONFIG_H
